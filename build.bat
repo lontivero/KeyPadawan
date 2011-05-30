c283786dbdea43248ef2520a234f44cf41d106e6
lontivero <lucasontivero@hotmail.com>
2011-05-30 17:07:08 -0300

    -Sort description in readme file and minor update to build.bat

--- build.bat
+++ build.bat
@@ -1,2 +1,12 @@
-%systemRoot%\Microsoft.NET\Framework64\v4.0.30319\MSbuild Keypadawan.sln /p:Configuration=Debug /p:Platform="Any CPU"
+set MicrosoftDotNet=%systemRoot%\Microsoft.NET
+
+if %PROCESSOR_ARCHITECTURE%==x86 (
+	set DotNetFramework=Framework
+) else (
+	set DotNetFramework=Framework64
+)
+
+set DotNetFrameworkVersionPath=%MicrosoftDotNet%\%DotNetFramework%\v4.0.30319
+
+%DotNetFrameworkVersionPath%\MSbuild Keypadawan.sln /p:Configuration=Debug /p:Platform="Any CPU"
 
