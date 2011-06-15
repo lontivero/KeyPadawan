4fc1aa8ce9568de8f501666647a07742a4ca4cad
Lucas Ontivero <lucasontivero@hotmail.com>
2011-06-14 23:26:28 -0300

    - Normal mode; Raw Mode; Shortcut mode

---
+++ Dev/View/ProcessorsBuilder.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using KeyPadawan.View.KeyProcessors;
+using KeyPadawan.Properties;
+
+
+namespace KeyPadawan.View
+{
+    class ProcessorsBuilder : IProcessorsBuilder
+    {
+        public IEnumerable<IEventProcessor> Build()
+        {
+            var settings = Settings.Default;
+            if (settings.ShortcutsMode) return new[] { new ShortcutProcessor() };
+            if (settings.RawMode) return new[] { new RawProcessor() };
+            if (settings.NormalMode) 
+            return new IEventProcessor[] {
+                new PasswordProcessor(),
+                new SymbolicKeysTranslator(),
+                new ShortcutProcessor(),
+                new CharKeyProcessor(),
+                new RawProcessor() };
+
+            return Enumerable.Empty<IEventProcessor>();
+        }
+    }
+}
